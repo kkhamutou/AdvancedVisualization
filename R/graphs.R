8e23dcad884a4d02b396b1405b73384daffdab76
Khamutou <kkhamutou@luxoft.com>
2020-01-17 00:59:28 +0100

    Local PC

--- R/graphs.R
+++ R/graphs.R
@@ -6,7 +6,7 @@ require(tidyverse)
 require(RColorBrewer)
 
 rdiff <- function(x) {
-  mean(diff(x)/x[-length(x)]) * 100
+  mean(diff(x)/x[-length(x)], na.rm = TRUE) * 100
 }
 
 add_theme <- function() {
@@ -22,10 +22,10 @@ add_theme <- function() {
   )
 }
 
-add_text_annotate <- function(df, x_margin=0, y_margin=0) {
+add_text_annotate <- function(df, x_margin=0, y_margin=0, h_just=0) {
   
   annotate(geom="text", x=df$date + x_margin, y=df$price + y_margin, label=df$label,
-           color=df$color, hjust = 0)
+           color=df$color, hjust = h_just)
 }    
 
 build_start_df <- function(df) {
@@ -85,8 +85,7 @@ plot.timeseries <- function(time_series, column_names, codes=NULL, collapse="2 w
     reduce(full_join, by="Date") %>%
     select(Date, matches(paste("", sep="_", codes, collapse="|"))) %>%
     select(Date, matches(sprintf("^(%s)", paste(column_names, collapse="|"))))
-  
-  # colors <- brewer.pal(n = 3, name = "Set2")
+
   min_date <- min(joined_series$Date)
   max_date <- max(joined_series$Date)
  
@@ -109,13 +108,13 @@ plot.timeseries <- function(time_series, column_names, codes=NULL, collapse="2 w
     scale_colour_brewer(palette="Set2") + 
     geom_line(size=1.2) +  
     scale_y_continuous(position="right") +
-    scale_x_date(date_breaks = "2 weeks", date_labels = "%b %Y") + 
+    scale_x_date(date_breaks = collapse, date_labels = "%b %Y") + 
     ggtitle(plot_title) + 
     theme(legend.position = "none") + 
     add_theme()
     
     for (row in 1:nrow(start_point)) {
-      p <- p + add_text_annotate(start_point[row, ], x_margin=-4, row) + 
+      p <- p + add_text_annotate(start_point[row, ], x_margin=-10, row, h_just=0.5) + 
         add_text_annotate(end_point[row, ], x_margin=1, 0.3)
     }
   
@@ -132,31 +131,115 @@ plot.timeseries <- function(time_series, column_names, codes=NULL, collapse="2 w
   p
 }
 
-
-
-plot.barcharts <- function(time_series, codes=NULL, column_names, collapse="2 weeks", func=NULL, ...) {
+#' Plot Quandl bar chart
+#'
+#' Plot bar chart from the Quandl Dataset.
+#' @param time_series List of data.frame retrieved from Quandl Dataset.
+#' @param column_names 	String or list of strings of titles for each column in this time-series. Map one or more columns from each time-sereis
+#' @param codes String or list of strings, code a.k.a ticker symbol on Quandle to be plotted. If NULL, then all codes are being included into a plot.
+#' @param func If FALSE, STOP is a code for a predefined date range is not found, otherwise WARNIGN. Default=FALSE
+#' @param ... Other arguments passed on to methods. Not currently used.
+#' @return a ggplot object
+#' @examples \dontrun{
+#' # Plot only only High and Low prices for each code (symbol) 
+#' plot.barchart (time_series=time_series, column_names=c("High", "Low"), codes=c("FB", "AAPL"))
+#' 
+#' # Plot only only Open and Close prices for Facebook (FB) and Apple (AAPL)
+#' plot.barchart (time_series=time_series, column_names=c("Open", "Close"), codes=c("FB", "AAPL"))
+#' 
+#' # Plot only only Open and Close prices for Facebook (FB) and Apple (AAPL) and add square root function
+#' plot.barchart (time_series=time_series, column_names=c("Open", "Close"), codes=c("FB", "AAPL"), func=y~sqrt(x))
+#' }
+#' @export
+plot.barchart <- function(time_series, column_names, codes=NULL, func=NULL, ...) {
   
   if (is.null(codes)) {
     codes <- names(time_series)
   }
   
-  joined_series <- x %>% 
+  joined_series <- time_series %>% 
     reduce(full_join, by="Date") %>%
     select(Date, matches(paste("", sep="_", codes, collapse="|"))) %>%
     select(Date, matches(sprintf("^(%s)", paste(column_names, collapse="|"))))
   
-  func_res <- lapply(joined_series[, -1], FUN = rdiff)
+  min_date <- min(joined_series$Date)
+  max_date <- max(joined_series$Date)
+  
+  plot_title <- sprintf("Symbol: %s | Price: %s | Start: %s | End: %s | Function: %s (mean)", 
+                        paste(codes, collapse="; "), 
+                        paste(column_names, collapse="; "), 
+                        min_date, 
+                        max_date, 
+                        ifelse(is.null(func), "Rate of Return", deparse(func)))
+  
+  func_res <- lapply(joined_series[, -1], FUN =  ifelse(is.null(func), rdiff, func))
   df <- data.frame(matrix(unlist(strsplit(names(func_res), "_")), ncol=2, byrow=TRUE), as.numeric(unlist(func_res)))
   names(df) <- c("Price", "Code", "Func")
   
   p <- ggplot(data=df, aes(x=Code, y=Func, fill=Price)) + 
     geom_bar(stat="identity", position=position_dodge(), width =0.8) + 
     geom_text(aes(label=round(Func, 2)), position=position_dodge(width=0.9), color="white") + 
-    scale_fill_brewer(palette="Blues") +
+    ggtitle(plot_title) +
+    scale_fill_brewer(palette="YlOrRd") +
     add_theme() + 
     theme(legend.position="bottom", 
-          legend.background=element_rect(fill="#101040"), 
-          legend.text = element_text(color="white")) + 
-    guides(colour = guide_legend(title.position = "left"))
+          legend.background=element_rect(fill="black"), 
+          legend.text = element_text(color="white"))
   p
+}
+
+#' Plot Quandl pie chart
+#'
+#' Plot pie chart from the Quandl Dataset.
+#' @param time_series List of data.frame retrieved from Quandl Dataset.
+#' @param w 	List of numbers, weights of stocks. If less then 1, "Other" code will be created.
+#' @param codes String or list of strings, code a.k.a ticker symbol on Quandle to be plotted. If NULL, then all codes are being included into a plot.
+#' @param ... Other arguments passed on to methods. Not currently used.
+#' @return a ggplot object
+#' @examples \dontrun{
+#' # Plot only only Open and Close prices for Facebook (FB) and Apple (AAPL)
+#' plot_piechart (time_series=time_series, w=c(0.3, 0.7), codes=c("FB", "AAPL"))
+#' 
+#' plot.barchart (time_series=time_series, w=c(0.3, 0.3, 0.3), codes=c("FB", "AAPL", "GOOG"))
+#' }
+#' @export
+plot_piechart <- function(time_series, w, codes=NULL, ...) {
+  
+  if (is.null(codes)) {
+    codes <- names(time_series)
+  }
+  
+  if (length(codes) != length(w))  {
+    stop("Length of codes and weights shoud be the same!")
+  }
+
+  if (sum(w) > 1) {
+    stop("Sum of weights should not be greate then 1!")
+  }
+  
+  if (sum(w) < 1) {
+    w <- append(w, 1-sum(w))
+    codes <- append(codes, 'Other')
+  }
+  
+  data <- data.frame(codes=codes, weights=w)
+  
+  data <- data %>% 
+    arrange(desc(codes)) %>%
+    mutate(prop = w / sum(data$w) *100) %>%
+    mutate(ypos = cumsum(prop)- 0.5*prop )
+  
+  p <- ggplot(data, aes(x="", y=prop, fill=codes)) + 
+    geom_bar(stat="identity", width=1) +
+    scale_fill_brewer(palette="Set1") +
+    geom_text(aes(y = ypos, label = codes), color = "white", size=6) + 
+    coord_polar(theta = "y", start=0) + 
+    add_theme()
+  p <- p +
+    theme(legend.position="none", 
+          axis.text = element_blank(),
+          axis.ticks = element_blank(),
+          panel.grid  = element_blank())
+  p
+  
 }
