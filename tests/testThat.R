72502056dd788ff319456805cd8f3785b4ec0e6e
Khamutou <kkhamutou@luxoft.com>
2020-01-17 10:11:38 +0100

    Local PC

--- tests/testThat.R
+++ tests/testThat.R
@@ -1,3 +1,6 @@
+source("R/graphs.R")
+source("R/market_data_api.R")
+
 require(ggplot2)
 require(grid)
 require(dplyr)
@@ -24,19 +27,19 @@ data$FB
 names(data$FB)
 
 # Open price for all
-plot_timeseries(data, column_names = c("Open"), collapse = "2 months")
+AdvancedVisualization::plot_timeseries(data, column_names = c("Open"), collapse = "2 months")
 
 # Close price for all
-plot_timeseries(data, column_names = c("Close"), collapse = "1 month")
+AdvancedVisualization::plot_timeseries(data, column_names = c("Close"), collapse = "1 month")
 
 # Close and Open
-plot_timeseries(data, column_names = c("High", "Low"), collapse = "2 months")
+AdvancedVisualization::plot_timeseries(data, column_names = c("High", "Low"), collapse = "2 months")
 
 # Close and Open
-plot_timeseries(data, column_names = c("High", "Low"), codes=c("FB", "AAPL"), collapse = "2 months")
+AdvancedVisualization::plot_timeseries(data, column_names = c("High", "Low"), codes=c("FB", "AAPL"), collapse = "2 months")
 
 # Add function
-plot_timeseries(data, column_names = c("Open"), codes=c("FB", "AAPL"), collapse = "2 months", func = y~sqrt(x))
+AdvancedVisualization::plot_timeseries(data, column_names = c("Open"), codes=c("FB", "AAPL"), collapse = "2 months", func = y~sqrt(x))
 
 # New dataset
 code <- c("GOOG", "INTC", "KO", "A")
@@ -45,15 +48,15 @@ data <- get.market.data(code, start_date, end_date, collapse, api_key)
 plot_timeseries(data, column_names = c("Open"), collapse = "2 months")
 
 # Plot all
-plot_timeseries_all(data, column_names = c("Open"), by="codes", func = y~sqrt(x))
-plot_timeseries_all(data, column_names = c("Volume"), by="codes")
+AdvancedVisualization::plot_timeseries_all(data, column_names = c("Open"), by="codes", func = y~sqrt(x))
+AdvancedVisualization::plot_timeseries_all(data, column_names = c("Volume"), by="codes")
 
-plot_timeseries_all(data, column_names = c("Close", "Open", "High", "Low"), by="column_names", codes = "GOOG")
+AdvancedVisualization::plot_timeseries_all(data, column_names = c("Close", "Open", "High", "Low"), by="column_names", codes = "GOOG")
 
 
 code <- c("C", "JPM", "BAC", "WFC")
 data <- get.market.data(code, start_date, end_date, collapse, api_key)
 
-plot_barchart(data, column_names=c("Open", "Open", "High", "Low"))
+AdvancedVisualization::plot_barchart(data, column_names=c("Open", "Open", "High", "Low"))
 
-plot_piechart(data, w=c(0.2, 0.2,0.2, 0.2))
+AdvancedVisualization::plot_piechart(data, w=c(0.2, 0.2,0.2, 0.2))
